d3ee689c6a8394573881a77fa8c633bea1426b12
Travis Matthews <tmatthew@e1z2r6p13.42.us.org>
2019-01-01 17:53:17 -0800

    writing closure func for building canonical collection

--- misc/build_parse_table.py
+++ misc/build_parse_table.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
 
 epsilon_set = {"EPS"}
-terminal_set = {"(", ")", "NUM", "NAME", "+", "-", "X", "/", "EOF", "EPS"}
-nonterminal_set = {"s", "expr", "expr'", "term", "term'", "factor"}
 
-def read_rules(filename, rules):
+def read_rules(filename, rules, terminal_set, nonterminal_set):
 	with open(filename) as file:
-		rules_text = file.read().split("\n")
-		for rule in rules_text:
-			sym, deriv_text = rule.split(":", 2) 
+		productions = file.read().split("\n")
+		for production in productions:
+			sym, deriv_text = production.split(":", 2) 
 			derivs = [[deriv for deriv in derivs_text.split()]
 				for derivs_text in deriv_text.split(",")]
 			rules[sym] = derivs
+			if sym.islower() and sym not in nonterminal_set:
+				nonterminal_set.add(sym)
+			elif not sym.islower() and sym not in terminal_set:
+				terminal_set.add(sym)
+			for deriv in derivs:
+				for symbol in deriv:
+					if symbol.islower() and symbol not in nonterminal_set:
+						nonterminal_set.add(symbol)
+					elif not symbol.islower() and symbol not in terminal_set:
+						terminal_set.add(symbol)
 
 def build_first_sets(rules, first):
 	for terminal in terminal_set:
@@ -37,9 +45,23 @@ def build_first_sets(rules, first):
 					first[rule] = first[rule] | rhs
 					changed = True
 
+def construct_cc_0(cc_0)
+	cc_0.insert(0, "s")
+	cc_0.insert(1, rules["s"])
+	cc_0[1].insert(0, ".")
+	cc_0.insert(2, "eof")
+
+def construct_canonical_collection(rules, terminal_set, nonterminal_set, canonical_collection)
+	cc_0 = []
+	construct_cc_0(cc_0)
+
 if __name__ == "__main__":
 	rules = {}
 	first_sets = {}
-	read_rules("misc/simple_grammar.txt", rules)
+	terminal_set = set()
+	nonterminal_set = set()
+	read_rules("misc/paren_grammar.txt", rules, terminal_set, nonterminal_set)
 	build_first_sets(rules, first_sets)
+	canonical_collection = []
+	construct_canonical_collection(rules, terminal_set, nonterminal_set, canonical_collection)
 	print(first_sets)
