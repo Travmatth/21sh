857caaffff2b5007292b6a23e8f7cf59cb15021e
Travis Matthews <tmatthew@e1z3r8p5.42.us.org>
2019-04-29 12:37:06 -0700

    properly exit and adding redir test cases, closes #19, close #21, close #22, close #23, close #24, close #25, close #26, close #27, close #28

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/23 20:06:46 by tmatthew          #+#    #+#             */
-/*   Updated: 2019/04/28 18:29:45 by tmatthew         ###   ########.fr       */
+/*   Updated: 2019/04/29 12:27:54 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,15 @@ int		main(int argc, char **argv, char **environ)
 {
 	char	*input;
 	int		status;
+	int		end;
 	
 	if (ERR(init_parser())
 		|| ERR(init_environ(argc, argv, environ))
 		|| ERR(init_shell()))
 		return (1);
+	end = FALSE;
 	status = SUCCESS;
-	while (!ERR(status))
+	while (!end && !ERR(status))
 	{
 		input = NULL;
 		if ((signal(SIGINT, sig_handler) != SIG_ERR)
@@ -36,9 +38,9 @@ int		main(int argc, char **argv, char **environ)
 			&& (OK(get_next_line(STDIN, &input))))
 		{
 			status = IS_A("exit", input) ? NIL : parse_execute_input(input);
-			free(input);
 			if (IS_A("exit", input))
-				break ;
+				end = TRUE;
+			free(input);
 		}
 	}
 	return (0);
