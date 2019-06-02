ecd6c32075196cfd3bdc83a34c988f6a365e53a7
Travis Matthews <tmatthew@e1z3r7p25.42.us.org>
2019-06-02 11:45:52 -0700

    finished wrod movement

--- src/interface/move.c
+++ src/interface/move.c
@@ -6,7 +6,7 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 19:31:47 by dysotoma          #+#    #+#             */
-/*   Updated: 2019/06/01 19:14:24 by tmatthew         ###   ########.fr       */
+/*   Updated: 2019/06/02 10:58:29 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,43 @@
 
 void		move_word_left(char **line, t_interface *ui)
 {
-	// if (ui->line_index)
-	// 	ui->line_index--;
-	// if currently inside word, move to first space preceding
-	// while (ui->line_index && !IS_WHITESPACE((*line)[ui->line_index]))
-	// {
-	// 	tputs(tgetstr("le", NULL), 1, ft_termprint);
-	// 	ui->line_index--;
-	// }
-	// if not beginning of line, move past all whitespaces
-	// while (ui->line_index && IS_WHITESPACE((*line)[ui->line_index]))
-	// {
-	// 	tputs(tgetstr("le", NULL), 1, ft_termprint);
-	// 	ui->line_index--;
-	// }
-	// if not beginning of line, move to beginning of word
-	// while ((ui->line_index)
-	// 	&& (ui->line_index - 1)
-	// 	&& !IS_WHITESPACE((*line)[ui->line_index]))
-	// {
-	// 	tputs(tgetstr("le", NULL), 1, ft_termprint);
-	// 	ui->line_index--;
-	// }
+	if (ui->line_index == ui->line_len)
+	{
+		tputs(tgetstr("le", NULL), 1, ft_termprint);
+		ui->line_index--;
+	}
+	while (ui->line_index && !IS_WHITESPACE((*line)[ui->line_index]))
+	{
+		tputs(tgetstr("le", NULL), 1, ft_termprint);
+		ui->line_index--;
+	}
+	while (ui->line_index && IS_WHITESPACE((*line)[ui->line_index]))
+	{
+		tputs(tgetstr("le", NULL), 1, ft_termprint);
+		ui->line_index--;
+	}
+	while ((ui->line_index > 0)
+		&& !IS_WHITESPACE((*line)[ui->line_index]))
+	{
+		tputs(tgetstr("le", NULL), 1, ft_termprint);
+		ui->line_index--;
+	}
 }
 
 void		move_word_right(char **line, t_interface *ui)
 {
-	(void)line;
-	(void)ui;
 	// if word is not end of line, move to space after word
+	while ((ui->line_index != ui->line_len) && !IS_WHITESPACE((*line)[ui->line_index]))
+	{
+		tputs(tgetstr("nd", NULL), 1, ft_termprint);
+		ui->line_index++;
+	}
 	// if space is not end of line, move past all spaces
+	while ((ui->line_index != ui->line_len) && IS_WHITESPACE((*line)[ui->line_index]))
+	{
+		tputs(tgetstr("nd", NULL), 1, ft_termprint);
+		ui->line_index++;
+	}
 }
 
 void		movement(unsigned long c, char **line, t_interface *interface)
