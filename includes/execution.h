23eff4eafb786718829ef7257ef3a46b82c2ed79
Travis Matthews <tmatthew@e1z2r8p11.42.us.org>
2019-06-11 22:42:28 -0700

    rm unneeded exports from execution.h

--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 17:07:20 by tmatthew          #+#    #+#             */
-/*   Updated: 2019/06/03 13:41:46 by tmatthew         ###   ########.fr       */
+/*   Updated: 2019/06/11 22:27:04 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,13 @@ int		redir_out_dup(t_redir *redir);
 */
 
 int		execute_switch(t_exec_node *node);
-int		execute(t_program *program);
 int		parse_execute_input(char *complete_cmd);
 
 /*
 ** src/execution/execute_nodes.c
 */
 
-int		exec_command(t_simple *simple, int should_exit);
 int		exec_simple_command(t_simple *simple);
-void	exec_pipe_parent_process(t_pipe *pipe_node, int pid, int pipe_fd[2]);
 int		exec_pipe(t_pipe *pipe_node);
 int		exec_logical(t_operator *operator);
 
@@ -50,8 +47,6 @@ int		exec_logical(t_operator *operator);
 */
 
 int		wait_loop(int pid, int *return_val);
-void	free_redirs(t_redir *redir);
-void	free_exec_node(t_exec_node *node);
 void	free_program(t_program *program);
 
 /*
@@ -75,7 +70,6 @@ int		redir_clobber(t_redir *redir);
 ** src/execution/redirs.c
 */
 
-int		redirs_switch(t_redir *redir);
 int		open_redirs(t_simple *simple);
 int		perform_redirs(t_simple *simple);
 #endif
