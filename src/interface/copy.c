5d3910365a20cecebb9c26e888bdb703c42d4161
Travis Matthews <tmatthew@e1z2r8p11.42.us.org>
2019-06-11 17:52:38 -0700

    copy_line

--- src/interface/copy.c
+++ src/interface/copy.c
@@ -6,7 +6,7 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 17:01:40 by tmatthew          #+#    #+#             */
-/*   Updated: 2019/06/10 17:02:14 by tmatthew         ###   ########.fr       */
+/*   Updated: 2019/06/11 17:51:46 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,17 @@ void	copy(t_interface *ui, char *buf, char *line)
 
 void	copy_line(t_interface *ui, char *buf, char *line)
 {
-	int		i;
+	t_uiline	*cur;
+	int			len;
 
-	if (!ui->select)
+	if (ui->select || !(cur = current_uiline(ui, ui->line_index)))
+	{
+		write(STDOUT, "\a", 1);
 		return ;
-	ui->select = FALSE;
-	write(STDOUT, STEADY_CURSOR, 5);
-	if (ERR(ui->ccp_start) || ERR(ui->ccp_end))
-		return ;
+	}
 	ft_strclr(buf);
-	i = ui->ccp_end - ui->ccp_start;
-	ft_memcpy(buf, &(line[ui->ccp_start]), i);
-	i = ui->ccp_start;
-	clear_all_lines(ui);
-	init_select(ui);
-	write_line(ui, line);
-	ui->line_index = 0;
-	set_cursor(ui, i);
+	set_cursor(ui, 0);
+	len = cur->end - cur->start;
+	len += line[cur->end] == '\n' ? 1 : 0;
+	ft_memcpy(buf, &(line[cur->start]), len);
 }
